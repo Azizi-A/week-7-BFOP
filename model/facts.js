3a3c783f77cdde1d8bb0b92cb108c33b9664e432
Azizi Adeyemo <azizi.adeyemo@gmail.com>
2020-08-27 11:15:07 +0100

    Added get random fact db connection
    Relates #17
    
    Co-authored-by: Aishah <aishah92@gmail.com>

--- model/facts.js
+++ model/facts.js
@@ -1,17 +1,19 @@
 const db = require("../database/connection");
 
-
 const createFact = data => {
   return db.query(
     "INSERT INTO users (owner_id, text_content, about_who) VALUES ($1, $2, $3)",
     [data.owner_id, data.text_content, data.about_who]
-  );//get $1 from jwt
+  ); //get $1 from jwt
   //error handling incase people leave out some values
 };
 
-
 const readFact = id => {
   return db.query("SELECT * FROM users WHERE username=($1)", [id]);
+};
+
+const readRandomFact = () => {
+  return db.query("SELECT * FROM posts ORDER BY RANDOM() LIMIT 1");
 };
 
 const readAllFacts = () => {
@@ -19,14 +21,24 @@ const readAllFacts = () => {
 };
 
 const updateFact = (content, fact_id) => {
-    return db.query("UPDATE facts SET text_content = $1 WHERE id = $2", [content, fact_id])
+  return db.query("UPDATE facts SET text_content = $1 WHERE id = $2", [
+    content,
+    fact_id,
+  ]);
 };
 
 const deleteFact = fact_id => {
-    return db.query("DELETE FROM facts WHERE id=$1", [fact_id])
+  return db.query("DELETE FROM facts WHERE id=$1", [fact_id]);
 };
 
 //get all posts about one person
 //get all posts wtih same owner id
 
-module.exports = { createFact, readFact, readAllFacts, updateFact, deleteFact };
+module.exports = {
+  createFact,
+  readFact,
+  readRandomFact,
+  readAllFacts,
+  updateFact,
+  deleteFact,
+};
